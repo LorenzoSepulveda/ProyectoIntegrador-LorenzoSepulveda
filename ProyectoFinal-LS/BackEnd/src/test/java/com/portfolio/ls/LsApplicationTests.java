1d70f534f1f64eb947afbafb3c1f635656e356bc
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-05-11 22:01:07 -0300

    commit

--- ProyectoFinal-LS/BackEnd/src/test/java/com/portfolio/ls/LsApplicationTests.java
+++ ProyectoFinal-LS/BackEnd/src/test/java/com/portfolio/ls/LsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.proyectointegrador.portfolio;
+package com.portfolio.ls;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PortfolioApplicationTests {
+class LsApplicationTests {
 
 	@Test
 	void contextLoads() {
